41445e3d94253446a771cd91af4f6d83c9b8cc6a
Eileen Lowers <funmaster22@hotmail.com>
2024-02-08 15:12:32 -0500

    setting up mondoDB and mongoose

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Toshokanime
 PS Capstone
+
+Planning: 
+https://trello.com/b/UIVMJCXR/toshokanime
+
+Backend:
+https://github.com/Castien/Toshokanime-backend
+
+Frontend:
+NOT YET MADE
--- index.js
+++ index.js
@@ -9,3 +9,12 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
+
+const mongoose = require('mongoose');
+
+mongoose.connect('URL', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.error('MongoDB connection error:', err));
